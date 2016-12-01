3ed1f41f3d8d2efce63fc0f33648e75cb7b04e04
Taylor Burgess <tayloraburgess@gmail.com>
2016-12-01 00:12:34 -0500

    Add basic templates and routes for all pages
    Add 'Companies', 'Posts', and 'Coaches' templates, and render them at
    appropriate routes in Flask.
    
    Add basic styling in main.css, and serve the stylesheet statically.

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('index.html') 
+
+@app.route('/companies')
+def companies():
+    return render_template('companies.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
+@app.route('/coaches')
+def coaches():
+    return render_template('coaches.html')
+
+@app.route('/css/<path:path>')
+def serve_css(path):
+    return send_from_directory('static/css', path)
 
 app.run()
